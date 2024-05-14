f82be9c2909ffab4193522ef7ad0557db78d74dc
Mirsada <96733900+Mvorfa@users.noreply.github.com>
2024-05-14 15:19:01 -0700

    Add project files.

---
+++ BookStore.App/Data/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using BookStore.App.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookStore.App.Data
+{
+    public class AppDbContext:DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options): base(options)
+        {
+             
+    }
+        public DbSet<Models.Book> Books { get; set; }
+
+    }
+}
